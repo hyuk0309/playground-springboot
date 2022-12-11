8ac66cfc4e95f040a35b7c02a189e1fc2eff4a7a
hyuk0309 <bahngeh@naver.com>
2022-12-11 21:08:05 +0900

    chore : init project

---
+++ src/test/java/com/elvis/springboot/PlaygroundSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elvis.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
